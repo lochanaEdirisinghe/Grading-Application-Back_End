f5bfc80deb503b9b8141defe8e57729ef2aa6fe7
Lochana Edirisinghe <edirisinghelochana5@gmail.com>
2021-02-21 07:03:24 +0530

    initial commit

---
+++ src/main/java/lk/lochana/gradingapplication/GradingApplication.java
@@ -0,0 +1,12 @@
+package lk.lochana.gradingapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GradingApplication.class, args);
+    }
+}
---
+++ src/test/java/lk/lochana/gradingapplication/GradingApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.lochana.gradingapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GradingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
